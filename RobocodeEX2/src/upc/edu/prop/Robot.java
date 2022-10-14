4a37c0647447a3bb81ab793bfe98fae08a379161
icorn1 <ixent.cornella@estudiantat.upc.edu>
2022-10-14 16:18:23 +0200

    Commit Inicial

---
+++ RobocodeEX2/src/upc/edu/prop/Robot.java
@@ -0,0 +1,24 @@
+package upc.edu.prop;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.*;
+import robocode.*;
+
+/**
+ *
+ * @author icorn & almogawer
+ */
+public class Robot extends AdvancedRobot{
+
+    public void run() {
+        
+        execute();
+    }
+    // Estrategia:
+    /*
+     * U
+     */
+    
+    
+}
