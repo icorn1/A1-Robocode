dfdc1e75dcbdb6ea843ac842a3dc4ed2780cac54
icorn1 <ixent.cornella@estudiantat.upc.edu>
2022-10-14 16:21:46 +0200

    RobocodeEX1

---
+++ RobocodeEX1/src/upc/edu/prop/RobotInutil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upc.edu.prop;
+
+import robocode.*;
+/**
+ *
+ * @author ixcor
+ */
+public class RobotInutil extends TeamRobot{
+    public void run() {
+        execute();
+    }
+}
